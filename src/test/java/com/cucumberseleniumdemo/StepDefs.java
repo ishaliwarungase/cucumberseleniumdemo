059d301ee8ac2ed91639af9c75dc00152655904c
ishaliwarungase <43536950+ishaliwarungase@users.noreply.github.com>
2021-02-01 15:52:47 +0530

    Update StepDefs.java

--- src/test/java/com/cucumberseleniumdemo/StepDefs.java
+++ src/test/java/com/cucumberseleniumdemo/StepDefs.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.JavascriptExecutor;
-
+import org.testng.Assert;
 
 import cucumber.api.java.Before;
 import cucumber.api.java.en.And;
