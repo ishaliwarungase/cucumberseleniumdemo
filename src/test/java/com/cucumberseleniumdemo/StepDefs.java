545e623180c5e4b439fe6f4da2213e4f91533545
ishaliwarungase <43536950+ishaliwarungase@users.noreply.github.com>
2021-02-01 13:49:15 +0530

    Update StepDefs.java

--- src/test/java/com/cucumberseleniumdemo/StepDefs.java
+++ src/test/java/com/cucumberseleniumdemo/StepDefs.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.JavascriptExecutor;
+import static org.junit.Assert.*;
 
 import cucumber.api.java.Before;
 import cucumber.api.java.en.And;
@@ -17,66 +18,66 @@ import cucumber.api.java.en.When;
 
 public class StepDefs 
 {
-    WebDriver driver;
-    @Before public void setUp()
-    { 
-        
-        System.setProperty("webdriver.chrome.driver", "/var/lib/jenkins/workspace/CucumberSeleniumDemo/src/test/java/com/cucumberseleniumdemo/chromedriver");
-        ChromeOptions options = new ChromeOptions().setHeadless(true);
-        driver = new ChromeDriver(options);
-    } 
+        WebDriver driver;
+        @Before public void setUp()
+        { 
 
-@Given("User enters URL")
-public void user_enters_url() 
-{
-    driver.get("http://aws-demo.shopizer.com/shop/");
+            System.setProperty("webdriver.chrome.driver", "/var/lib/jenkins/workspace/CucumberSeleniumDemo/src/test/java/com/cucumberseleniumdemo/chromedriver");
+            ChromeOptions options = new ChromeOptions().setHeadless(true);
+            driver = new ChromeDriver(options);
+        } 
 
-    driver.manage().timeouts().implicitlyWait(10000, TimeUnit.MILLISECONDS);
-
-    Actions actions = new Actions(driver);
-       
-    WebElement menuOption = driver.findElement(By.linkText("My Account"));
-     
-    actions.moveToElement(menuOption).perform();
-
-    WebElement subMenu = driver.findElement(By.xpath("/html/body/nav[1]/div/ul[2]/li[2]/ul/li[2]/a"));
-
-    JavascriptExecutor executor = (JavascriptExecutor)driver;
-
-    executor.executeScript("arguments[0].click();", subMenu);
-}
-
-@And("He enters userName")
-public void he_enters_userName() 
-{
-    driver.findElement(By.id("signin_userName")).sendKeys("abc@gmail.com");
-}
-
-@When("He enters password")
-public void he_enters_password() 
-{
-    driver.findElement(By.id("signin_password")).sendKeys("P@33w0rd");
-}
-
-@Then("Home page is displayed")
-public void home_page_is_displayed() 
-{
-    driver.findElement(By.id("genericLogin-button")).click();
-
-    String msg = driver.findElement(By.xpath('//*[@id="loginError"]')).getText();
-
-    String errmsg = "Login Failed. Username or Password is incorrect.";
-
-    assertEquals(msg,errmsg);
-
-    if(msg == errmsg)
+    @Given("User enters URL")
+    public void user_enters_url() 
     {
-        print('Login Failed. Username or Password is incorrect');
+        driver.get("http://aws-demo.shopizer.com/shop/");
+
+        driver.manage().timeouts().implicitlyWait(10000, TimeUnit.MILLISECONDS);
+
+        Actions actions = new Actions(driver);
+
+        WebElement menuOption = driver.findElement(By.linkText("My Account"));
+
+        actions.moveToElement(menuOption).perform();
+
+        WebElement subMenu = driver.findElement(By.xpath("/html/body/nav[1]/div/ul[2]/li[2]/ul/li[2]/a"));
+
+        JavascriptExecutor executor = (JavascriptExecutor)driver;
+
+        executor.executeScript("arguments[0].click();", subMenu);
     }
-    else
+
+    @And("He enters userName")
+    public void he_enters_userName() 
     {
-        print('Home page is displayed');
+        driver.findElement(By.id("signin_userName")).sendKeys("abc@gmail.com");
     }
-    driver.close();
-}
+
+    @When("He enters password")
+    public void he_enters_password() 
+    {
+        driver.findElement(By.id("signin_password")).sendKeys("P@33w0rd");
+    }
+
+    @Then("Home page is displayed")
+    public void home_page_is_displayed() 
+    {
+        driver.findElement(By.id("genericLogin-button")).click();
+
+        String msg = driver.findElement(By.xpath('//*[@id="loginError"]')).getText();
+
+        String errmsg = "Login Failed. Username or Password is incorrect.";
+
+        assertEquals(msg,errmsg);
+
+        if(msg == errmsg)
+        {
+            System.out.println('Login Failed. Username or Password is incorrect');
+        }
+        else
+        {
+            System.out.println('Home page is displayed');
+        }
+        driver.close();
+    }
 }
